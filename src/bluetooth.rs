e9a9014dac4bd62f72802ac01d26d2aeb9c8e5e8
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-09-25 16:52:03 +0530

    bluetooth with cli

---
+++ src/bluetooth.rs
@@ -0,0 +1,149 @@
+use bluer::{Adapter, AdapterEvent, Address, DeviceEvent, DiscoveryFilter, DiscoveryTransport};
+use futures::{pin_mut, stream::SelectAll, StreamExt};
+use std::{collections::HashSet, env};
+use std::error::Error;
+
+pub struct BluetoothSDK {
+    adapter: Adapter,
+}
+
+async fn query_device(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
+    let device = adapter.device(addr)?;
+    println!("    Address type:       {}", device.address_type().await?);
+    println!("    Name:               {:?}", device.name().await?);
+    println!("    Icon:               {:?}", device.icon().await?);
+    println!("    Class:              {:?}", device.class().await?);
+    println!("    UUIDs:              {:?}", device.uuids().await?.unwrap_or_default());
+    println!("    Paired:             {:?}", device.is_paired().await?);
+    println!("    Connected:          {:?}", device.is_connected().await?);
+    println!("    Trusted:            {:?}", device.is_trusted().await?);
+    println!("    Modalias:           {:?}", device.modalias().await?);
+    println!("    RSSI:               {:?}", device.rssi().await?);
+    println!("    TX power:           {:?}", device.tx_power().await?);
+    println!("    Manufacturer data:  {:?}", device.manufacturer_data().await?);
+    println!("    Service data:       {:?}", device.service_data().await?);
+    Ok(())
+}
+
+async fn query_all_device_properties(adapter: &Adapter, addr: Address) -> bluer::Result<()> {
+    let device = adapter.device(addr)?;
+    let props = device.all_properties().await?;
+    for prop in props {
+        println!("    {:?}", &prop);
+    }
+    Ok(())
+}
+
+impl BluetoothSDK {
+    pub async fn new() -> Result<Self, Box<dyn Error>> {
+        let adapter = Adapter::default().await?;
+        Ok(Self { adapter })
+    }
+
+    pub async fn turn_on_bluetooth(&self) -> Result<(), Box<dyn Error>> {
+        self.adapter.set_powered(true).await?;
+        Ok(())
+    }
+
+    pub async fn turn_off_bluetooth(&self) -> Result<(), Box<dyn Error>> {
+        self.adapter.set_powered(false).await?;
+        Ok(())
+    }
+
+    pub async fn scan_bluetooth(&self) -> Result<(), Box<dyn Error>> {
+        let with_changes = env::args().any(|arg| arg == "--changes");
+        let all_properties = env::args().any(|arg| arg == "--all-properties");
+        let le_only = env::args().any(|arg| arg == "--le");
+        let br_edr_only = env::args().any(|arg| arg == "--bredr");
+        let filter_addr: HashSet<_> = env::args().filter_map(|arg| arg.parse::<Address>().ok()).collect();
+    
+        env_logger::init();
+        let session = bluer::Session::new().await?;
+        let adapter = session.default_adapter().await?;
+        println!("Discovering devices using Bluetooth adapter {}\n", adapter.name());
+        adapter.set_powered(true).await?;
+    
+        let filter = DiscoveryFilter {
+            transport: if le_only {
+                DiscoveryTransport::Le
+            } else if br_edr_only {
+                DiscoveryTransport::BrEdr
+            } else {
+                DiscoveryTransport::Auto
+            },
+            ..Default::default()
+        };
+        adapter.set_discovery_filter(filter).await?;
+        println!("Using discovery filter:\n{:#?}\n\n", adapter.discovery_filter().await);
+    
+        let device_events = adapter.discover_devices().await?;
+        pin_mut!(device_events);
+    
+        let mut all_change_events = SelectAll::new();
+    
+        loop {
+            tokio::select! {
+                Some(device_event) = device_events.next() => {
+                    match device_event {
+                        AdapterEvent::DeviceAdded(addr) => {
+                            if !filter_addr.is_empty() && !filter_addr.contains(&addr) {
+                                continue;
+                            }
+    
+                            println!("Device added: {addr}");
+                            let res = if all_properties {
+                                query_all_device_properties(&adapter, addr).await
+                            } else {
+                                query_device(&adapter, addr).await
+                            };
+                            if let Err(err) = res {
+                                println!("    Error: {}", &err);
+                            }
+    
+                            if with_changes {
+                                let device = adapter.device(addr)?;
+                                let change_events = device.events().await?.map(move |evt| (addr, evt));
+                                all_change_events.push(change_events);
+                            }
+                        }
+                        AdapterEvent::DeviceRemoved(addr) => {
+                            println!("Device removed: {addr}");
+                        }
+                        _ => (),
+                    }
+                    println!();
+                }
+                Some((addr, DeviceEvent::PropertyChanged(property))) = all_change_events.next() => {
+                    println!("Device changed: {addr}");
+                    println!("    {property:?}");
+                }
+                else => break
+            }
+        }
+    
+        Ok(())
+    }
+
+    pub async fn connect_bluetooth(&self, device_address: &str) -> Result<(), Box<dyn Error>> {
+        self.adapter.start_discovery().await?;
+
+        let mut events = self.adapter.events().await?;
+        while let Some(event) = events.recv().await {
+            match event {
+                BluetoothEvent::Device { address, .. } if address == device_address => {
+                    self.adapter.stop_discovery().await?;
+
+                    let device = Device::new(self.adapter.session(), &address).await?;
+                    device.connect().await?;
+
+                    println!("Connected to device {}", device_address);
+                    
+                    return Ok(());
+                }
+                _ => {}
+            }
+        }
+
+        Err("Device not found".into())
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,40 @@
-use bluer::{Adapter, BluetoothEvent};
+use clap::{App, Arg, SubCommand};
+use tokio::runtime::Runtime;
 
-async fn scan_devices(adapter: &Adapter) -> Result<(), Box<dyn std::error::Error>> {
-    // Start discovery on the adapter
-    adapter.start_discovery().await?;
+mod bluetooth;
+use bluetooth::BluetoothSDK;
+fn main() {
+    let matches = App::new("Bluetooth CLI")
+        .version("1.0")
+        .author("Your Name <your.email@example.com>")
+        .about("Interacts with Bluetooth devices")
+        .subcommand(SubCommand::with_name("on").about("Turns on Bluetooth"))
+        .subcommand(SubCommand::with_name("off").about("Turns off Bluetooth"))
+        .subcommand(SubCommand::with_name("scan").about("Scans for Bluetooth devices"))
+        .subcommand(
+            SubCommand::with_name("connect")
+                .about("Connects to a Bluetooth device")
+                .arg(Arg::with_name("ADDRESS").required(true).index(1)),
+        )
+        .get_matches();
 
-    // Listen for Bluetooth events
-    let mut events = adapter.events().await?;
-    
-    while let Some(event) = events.recv().await {
-        match event {
-            // When a new device is discovered
-            BluetoothEvent::Device { address, name, .. } => {
-                println!("Discovered device: {} ({})", name.unwrap_or_default(), address);
-            }
-            _ => {}
+    let mut rt = Runtime::new().unwrap();
+    let sdk = rt.block_on(BluetoothSDK::new()).unwrap();
+
+    match matches.subcommand() {
+        ("on", Some(_)) => {
+            BluetoothSDK::turn_on_bluetooth(&sdk).unwrap();
         }
+        ("off", Some(_)) => {
+           BluetoothSDK::turn_off_bluetooth(&sdk).unwrap();
+        }
+        ("scan", Some(_)) => {
+           BluetoothSDK::scan_bluetooth(&sdk).unwrap();
+        }
+        ("connect", Some(connect_matches)) => {
+            let address = connect_matches.value_of("ADDRESS").unwrap();
+            rt.block_on(sdk.connect_bluetooth(address)).unwrap();
+        }
+        _ => {}
     }
-
-    Ok(())
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Get the default Bluetooth adapter
-    let adapter = Adapter::default().await?;
-
-    // Scan for devices
-    scan_devices(&adapter).await?;
-
-    Ok(())
 }
