3950e1e6735e3350c8c82bb9ea0705d644ce8c65
Dhruvesh08 <dhruveshbaria@gmail.com>
2023-09-25 16:37:33 +0530

    scan bluetooth devices

--- src/main.rs
+++ src/main.rs
@@ -1,47 +1,32 @@
-use std::env;
+use bluer::{Adapter, BluetoothEvent};
 
-async fn query_adapter(adapter: &bluer::Adapter) -> bluer::Result<()> {
-    println!("    Address:                    {}", adapter.address().await?);
-    println!("    Address type:               {}", adapter.address_type().await?);
-    println!("    Friendly name:              {}", adapter.alias().await?);
-    println!("    Modalias:                   {:?}", adapter.modalias().await?);
-    println!("    Powered:                    {:?}", adapter.is_powered().await?);
-    println!("    Discoverabe:                {:?}", adapter.is_discoverable().await?);
-    println!("    Pairable:                   {:?}", adapter.is_pairable().await?);
-    println!("    UUIDs:                      {:?}", adapter.uuids().await?);
-    println!();
-    println!("    Active adv. instances:      {}", adapter.active_advertising_instances().await?);
-    println!("    Supp.  adv. instances:      {}", adapter.supported_advertising_instances().await?);
-    println!("    Supp.  adv. includes:       {:?}", adapter.supported_advertising_system_includes().await?);
-    println!("    Adv. capabilites:           {:?}", adapter.supported_advertising_capabilities().await?);
-    println!("    Adv. features:              {:?}", adapter.supported_advertising_features().await?);
+async fn scan_devices(adapter: &Adapter) -> Result<(), Box<dyn std::error::Error>> {
+    // Start discovery on the adapter
+    adapter.start_discovery().await?;
 
-    Ok(())
-}
-
-async fn query_all_adapter_properties(adapter: &bluer::Adapter) -> bluer::Result<()> {
-    let props = adapter.all_properties().await?;
-    for prop in props {
-        println!("    {:?}", &prop);
-    }
-    Ok(())
-}
-
-#[tokio::main(flavor = "current_thread")]
-async fn main() -> bluer::Result<()> {
-    let all_properties = env::args().any(|arg| arg == "--all-properties");
-
-    let session = bluer::Session::new().await?;
-    let adapter_names = session.adapter_names().await?;
-    for adapter_name in adapter_names {
-        println!("Bluetooth adapater {}:", &adapter_name);
-        let adapter = session.adapter(&adapter_name)?;
-        if all_properties {
-            query_all_adapter_properties(&adapter).await?;
-        } else {
-            query_adapter(&adapter).await?;
+    // Listen for Bluetooth events
+    let mut events = adapter.events().await?;
+    
+    while let Some(event) = events.recv().await {
+        match event {
+            // When a new device is discovered
+            BluetoothEvent::Device { address, name, .. } => {
+                println!("Discovered device: {} ({})", name.unwrap_or_default(), address);
+            }
+            _ => {}
         }
-        println!();
     }
+
+    Ok(())
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    // Get the default Bluetooth adapter
+    let adapter = Adapter::default().await?;
+
+    // Scan for devices
+    scan_devices(&adapter).await?;
+
     Ok(())
 }
